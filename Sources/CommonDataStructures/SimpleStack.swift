f0bd243cabf90dfb7eb138bd9bd03eda8cfe4cef
stixlee <michael.lee.1983@iclllud.com>
2023-12-27 09:28:22 -0700

    Added Simple Stack

--- Sources/CommonDataStructures/SimpleStack.swift
+++ Sources/CommonDataStructures/SimpleStack.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+public class SimpleStack<Element> {
+    
+    private var backingStore: [Element]
+    
+    public var count: Int { return backingStore.count }
+    
+    public var top: Element? {
+        guard backingStore.count > 0 else { return nil }
+        return backingStore.last
+    }
+    
+    public init(with array: [Element]) { backingStore = array }
+
+    public convenience init() { self.init(with: [Element]() ) }
+    
+    public func push(_ item: Element) { backingStore.append(item) }
+    
+    public func pop() -> Element? {
+        guard backingStore.count > 0 else { return nil }
+        let item = backingStore.last
+        backingStore.removeLast()
+        return item
+    }
+    
+    public func toArray() -> [Element] { return backingStore }
+    
+}
